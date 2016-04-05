9f82fafdb836f5580f379afbc74ed2a2343709da
Ion Toloaca <(none)>
2016-04-06 01:33:11 +0200

    Fix unicode issues

--- js/MathSemantifier.js
+++ js/MathSemantifier.js
@@ -9,14 +9,9 @@ function append(html) {
     $("#results").append(html);
 }
 
-function encodeAllURIComponents(str) {
-    return $.map(str.split(""), 
-        function(val, i) { 
-            return encodeURIComponent(val) 
-        }).join("");   
-}
-
-function processNotPositions() {
+function processNotPositions(data) {
+    data = JSON.parse(data)
+    data["message"] = decodeURIComponent(data["message"])
     console.log("Status: " + data["status"]);
     console.log("Message: " + data["message"]);
     console.log("Payload:");
@@ -34,36 +29,42 @@ function processNotPositions() {
             var info = 'key=\"' + key + '\" ';
             info += 'start=\"' + start + '\" ';
             info += 'length=\"' + length + '\" ';
-            var html = '<button class="notation_pos" ' + info + '>' + pos + '</button>'; // 
+            var html = '<button class="notation_pos" ' + info + '>' + pos + '</button>';
+            // 
             append(html);
             append('<span> </span>');
         }
     }
-    installNotationPosHandler();
+    installNotationPosHandler(data);
 }
 
-function installNotationPosHandler() {
+function installNotationPosHandler(data) {
     $('.notation_pos').click(function(e) {
         console.log(".notation_pos button pressed\n");
         var input = data["message"];
-        $.post(parsingEngineURL + "/get_arguments", {
-                input: encodeAllURIComponents(input),
-                key: $(this).attr('key'),
-                start: $(this).attr('start'),
-                length: $(this).attr('length')
-            },
-            displayArguments);
+        var url = parsingEngineURL + "/get_arguments";
+        var json = {
+            input: input,
+            key: $(this).attr('key'),
+            start: $(this).attr('start'),
+            length: $(this).attr('length')
+        }
+        $.post(url, json, displayArguments);
         console.log($(this).attr('key') + " " + $(this).attr('start') + " " + $(this).attr('length'));
     });
 }
 
 //{"status":"OK","payload":[{"position":[[0,52]],"argRuleN651A1ArgSeq":[[0,32],[42,10]]}],"key":"...","message":"..."}
 function displayArguments(arg) {
+    data = JSON.parse(arg);
+    data["message"] = decodeURIComponent(data["message"])
+    
     console.log("In display arguments\n");
     console.log(arg);
-    data = JSON.parse(arg);
     console.log(data);
+    
     $('#results').empty();
+    
     var resultPageHeader = data["status"] + "<br>" + data["message"] + "<br>";
     $("#results").append(resultPageHeader);
     $("#results").append('<div>' + data["key"] + '</div>');
@@ -86,20 +87,20 @@ function displayArguments(arg) {
             }
         }
     }
-    installDisplayArgsHandler();
+    installDisplayArgsHandler(data);
 }
 
-function installDisplayArgsHandler() {
+function installDisplayArgsHandler(data) {
     $('.argument_pos').click(function(e) {
         console.log(".argument_pos used\n");
         console.log(data);
         var payload = data.payload[$(this).attr('index')];
-        var requestData = JSON.stringify({
+        var requestData = encodeURIComponent(JSON.stringify({
             'status': 'OK',
             'payload': payload,
             'key': data.key,
-            'input': encodeAllURIComponents(data.message)
-        });
+            'input': data.message
+        }));
         console.log("Request data:\n");
         console.log(requestData);
         $.ajax({
@@ -116,6 +117,7 @@ function replaceArgs(data) {
     console.log("replaceArgs");
     console.log(data);
     var result = unescape(data.cml);
+    result = decodeURIComponent(result);
     console.log(data.cml);
     console.log(result);
     $("#editor").val(result);
